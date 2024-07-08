b971a3b7f7f4b6c5dd0815e4b02bf0fdb0720202
Bernabe Ortega-Tenezaca <bernabe.ortega@gmail.com>
2024-07-07 20:43:32 -0500

    Fix pilas

--- PilasApp/07_LIFO/01_TeoriaPilas.cs
+++ PilasApp/07_LIFO/01_TeoriaPilas.cs
@@ -1,17 +1,23 @@
-// Definición de una pila para enteros
-Stack<int> pilaEnteros = new Stack<int>();
-
-// Agregar elementos de la pila
-pilaEnteros.Push(1);
-pilaEnteros.Push(2);
-pilaEnteros.Push(100);
 
 
+public class Pila{
+    static void Run(){
+        // Definición de una pila para enteros
+        Stack<int> pilaEnteros = new Stack<int>();
 
-static void ImprimirPila(Stack<int> pila){
-    foreach (var item in pila)
-    {
-        System.Console.WriteLine(item);
+        // Agregar elementos de la pila
+        pilaEnteros.Push(1);
+        pilaEnteros.Push(2);
+        pilaEnteros.Push(100);
+
+
+
+        static void ImprimirPila(Stack<int> pila){
+            foreach (var item in pila)
+            {
+                System.Console.WriteLine(item);
+            }
+            System.Console.WriteLine(new string('-', 20));
+        }
     }
-    System.Console.WriteLine(new string('-', 20));
 }
