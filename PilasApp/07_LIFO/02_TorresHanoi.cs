982a49f15094f6e0bcb4171c8652f87830fa887e
Bernabe Ortega-Tenezaca <bernabe.ortega@gmail.com>
2024-07-07 21:54:41 -0500

    Definición de las torres de hanoi

--- PilasApp/07_LIFO/02_TorresHanoi.cs
+++ PilasApp/07_LIFO/02_TorresHanoi.cs
@@ -1,6 +1,9 @@
 public static class Hanoi{
 
+    static Stack<int> torreA = new Stack<int>();
+    static Stack<int> torreB = new Stack<int>();
+    static Stack<int> torreB = new Stack<int>();
     public static void Run(){
-        
+
     }
 }
