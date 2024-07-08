4bd3b1db59ebe30d50e700d51a67cb39703408a7
Bernabe Ortega-Tenezaca <bernabe.ortega@gmail.com>
2024-07-07 19:52:53 -0500

    Creación de menú

--- PilasApp/Program.cs
+++ PilasApp/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+menu();
+string? opcion = Console.ReadLine();
+
+switch (opcion)
+{
+    case "0":
+        return;
+    case "1":
+        System.Console.WriteLine("Ha seleccionado la opcioón 1");
+        break;
+    default:
+        System.Console.WriteLine("Opción no válida, presione una tecla para continuar");
+        break;
+}
+
+
+
+
+
+
+/**
+* Muestra el menú
+*/
+static void menu(){
+    caratula();
+    System.Console.WriteLine("1 Teoría de pilas");
+    System.Console.WriteLine("0 Salir");
+
+}
+
+/**
+*   Muestra la caratula general del ejercicio.
+*/
+static void caratula()
+{
+    // Console.Clear();
+    Console.WriteLine("\f\u001b\x1b[3J"); 
+    System.Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+    System.Console.WriteLine("+             UNIVERSIDAD ESTATAL AMAZÓNICA           +");
+    System.Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+    System.Console.WriteLine();
+    System.Console.WriteLine("           Menú de opciones - Escriba un número");
+    System.Console.WriteLine();
+
+}
