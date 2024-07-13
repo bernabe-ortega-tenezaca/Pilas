f8c1c764bab568a9efcfc86d8b8e5189a4ff5ebb
Bernabe Ortega-Tenezaca <bernabe.ortega@gmail.com>
2024-07-13 08:55:03 -0500

    fix imprimir pila

--- PilasApp/07_LIFO/03_Balanceo.cs
+++ PilasApp/07_LIFO/03_Balanceo.cs
@@ -1,4 +1,9 @@
 public static class Balanceo{
+    /* El siguiente código es demostrativo para la clase de Estructura de datos sobre el manejo de pilas, 
+    *  únicamente cuenta la cantidad de parentesis abiertos y cerrados de ecuaciones simples, ((4+3*(2+5))),
+    *  no revisa la estructura de la fórmula como por ejemplo (4+3*)2+5()())))
+    *  TODO: ampliar el algoritmo para resolver ecuaciones que tengan mas elementos como por ejemplo {[()]}
+    */
 
     public static void Run(){
         // (4+3*)2+5)))
@@ -35,13 +40,14 @@ public static class Balanceo{
         // }
 
         // if de una sola linea
-        string resultado = ParentesisAbiertos == ParentesisCerrados? "Balanceada": "No balanceada";
+        string resultado = ParentesisAbiertos == ParentesisCerrados? "Balanceada": "NO balanceada";
         System.Console.WriteLine("Ecuación " + resultado);
     }
 
 
     //Función para recorrer la pila e imprimir sus elementos
     static void imprimirPila(Stack<char> pila){
+        System.Console.WriteLine("Los elementos de la lista son: ");
         foreach (var item in pila) 
         {
             System.Console.WriteLine(item);
