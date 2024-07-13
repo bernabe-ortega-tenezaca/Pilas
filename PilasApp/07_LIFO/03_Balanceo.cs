be33d9b8f615a50aef2f29972a704b88f86b5763
Bernabe Ortega-Tenezaca <bernabe.ortega@gmail.com>
2024-07-13 08:12:52 -0500

    Funcion para imprimir pilas

--- PilasApp/07_LIFO/03_Balanceo.cs
+++ PilasApp/07_LIFO/03_Balanceo.cs
@@ -6,5 +6,22 @@ public static class balanceo{
         // Definimos una pila para almacenar letras
         Stack<char> ecuacion = new Stack<char>();
 
+        string formula = "(4+(3*(2+5)))";
+
+        foreach (char letra in formula)
+        {
+            ecuacion.Push(letra); // Pasamos cada letra de la formula a la pila de caracteres
+        }
+        imprimirPila(ecuacion);
+
+    }
+
+
+    //Función para recorrer la pila e imprimir sus elementos
+    static void imprimirPila(Stack<char> pila){
+        foreach (var item in pila) 
+        {
+            System.Console.WriteLine(item);
+        }
     }
 }
