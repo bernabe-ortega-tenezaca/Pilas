9c56dd88ba464c1dfb7aa22f922aa745a90e1976
Bernabe Ortega-Tenezaca <bernabe.ortega@gmail.com>
2024-07-20 08:19:17 -0500

    Simulacion de tiempos de atención por usuario

--- PilasApp/08_FIFO/02_Ejemplos.cs
+++ PilasApp/08_FIFO/02_Ejemplos.cs
@@ -3,6 +3,7 @@ public static class EjemplosCola{
         // Queue sirve para definir una cola
         // usuarios es una colección con strings
         Queue<string> usuarios = new Queue<string>();
+        Random rnd = new Random();
 
         // Enqueue sirve para encolar, agregar elementos a la cola
         usuarios.Enqueue("Ana"); // Ana es el elemento que entra primero
@@ -15,16 +16,26 @@ public static class EjemplosCola{
         usuarios.Dequeue();
         System.Console.WriteLine();
         imprimirCola(usuarios);
+        //Thread.Sleep(2000); // Pausa de 2000 milisegundos
         
         // llenar una cola con 50 usuarios
         for (int i = 1; i <= 50; i++)
         {
             usuarios.Enqueue("usuario_"+i.ToString()); // Eje: usuario_1, usuario_2 ....
         }
-        imprimirCola(usuarios);
+        System.Console.WriteLine();
+        //Thread.Sleep(3000);
+        //imprimirCola(usuarios);
 
+        // simular atención al cliente
+        while (usuarios.Count > 0)
+        {
+            System.Console.WriteLine("El usuario {0} ha sido atendido.", usuarios.Dequeue());
+            Thread.Sleep(rnd.Next(2000)); //
+        }
 
         //TODO: Cola prioritaria 
+        //TODO: Pasajes
     }
 
     private static void imprimirCola(Queue<string> cola){   
