69b773ccd0ab288a45e5ef403f21ae26bb44ecae
Bernabe Ortega-Tenezaca <bernabe.ortega@gmail.com>
2024-08-17 08:19:43 -0500

    método contains

--- PilasApp/09_HashSet/01_TeoriaConjuntos.cs
+++ PilasApp/09_HashSet/01_TeoriaConjuntos.cs
@@ -7,7 +7,25 @@ public static class Conjuntos{
         numeros.Add(2); 
         numeros.Add(3); // Agregamos el elmento 3 al conjunto
 
+        // bool existe = numeros.Contains(2);
+
+        // if(existe == true){
+        //     System.Console.WriteLine("El elemento 2 ya existe");
+        // }
+        // else{
+        //     System.Console.WriteLine("El elemento 2 no existe");
+        // }
+
+        // El método contains busca un elemento dentro del conjunto
+        if(numeros.Contains(3)){ // buscamos el elemento 3 dentro 
+            System.Console.WriteLine("El elemento 3 ya existe");
+        }
+        else{
+            System.Console.WriteLine("El elemento 3 no existe");
+        }
+
         //Imprimir el conjunto
+        System.Console.WriteLine("El contenido del conjunto es:");
         foreach (var item in numeros) // recorrer la colección numeros
         {
             Console.WriteLine(item);
