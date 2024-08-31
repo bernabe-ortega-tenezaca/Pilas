c81b3ac9b76dfc71cc2eebaea20d97596d41b8d0
Bernabe Ortega-Tenezaca <bernabe.ortega@gmail.com>
2024-08-31 08:16:12 -0500

    containskey

--- PilasApp/10_TareaDiccionario/Diccionario.cs
+++ PilasApp/10_TareaDiccionario/Diccionario.cs
@@ -1,24 +1,51 @@
 public static class Diccionario{
+
+    // Este ejemplo NO resuelve palabras con doble significado
+    // book => libro, reservación
+    // bug => gusano, error de programación
     public static void Run(){
         Dictionary<string, string> idioma = new Dictionary<string, string>();
 
         //Agregar palabras al diccionario
         idioma.Add("Hola", "Hello");
         idioma.Add("Adios", "bye");
+        // idioma.Add("book", "libro");
+        // idioma.Add("book", "reservación");
 
         //Agregar palabras al diccionario desde una variable
-        System.Console.WriteLine("Ingrese una nueva clave y valor al diccionario");
-        System.Console.WriteLine("Ingrese una clave");
-        string? clave = Console.ReadLine();
         //string clave = "bienvenido";
         //string valor = "welcome";
+
+        System.Console.WriteLine("Ingrese una nueva clave y valor al diccionario");
+        
+        //Agregar datos al diccionario desde teclado
+        System.Console.WriteLine("Ingrese una clave");
+        string? clave = Console.ReadLine();
         System.Console.WriteLine("Ingrese el valor asociado");
         string? valor = Console.ReadLine();
+        
+        //ContainsKey comprueba si una clave ya existe
+        if(idioma.ContainsKey(clave))
+        {
+            System.Console.WriteLine("La palabra ya existe");
+        }else{
+            idioma.Add(clave, valor);
+        }
 
-        idioma.Add(clave, valor);
+        
+        //Traductor
+        System.Console.WriteLine("Ingrese un texto a traducir");
+        string? texto = Console.ReadLine();
+
+        string[] palabras = texto.Split(" "); //convertimos el texto en un vector
 
         //imprimir contenido del diccionario
         foreach (var item in idioma)
+        {
+            System.Console.WriteLine(item);
+        }
+
+        foreach (var item in palabras)
         {
             System.Console.WriteLine(item);
         }
